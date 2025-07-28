df2d6c36e8f3e505145568f2350c4b6e6b1f07b1
AgenorIT <proyectoagenorsoft@gmail.com>
2025-07-27 23:54:46 -0300

    🔄 Deploy build para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-      <Hero />
+      {<Hero />}
       {<About /> }
       {<Experience /> }
       {<Skills />}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
+  // En DEV base="/", en BUILD base="/cv-hergro/"
   base: command === 'build' ? '/cv-hergro/' : '/',
   plugins: [react()],
-  preview: { open: '/cv-hergro/' },
+  // opcional: abre la ruta correcta en preview
+  preview: { open: '/cv-hergro/' }
 }))
