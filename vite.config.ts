811d4e5e80b9a5d5b2eea74a3e1568bfe3dc9da3
AgenorIT <proyectoagenorsoft@gmail.com>
2025-07-27 23:20:26 -0300

    🔄 Redeploy: usar index.html de producción

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/cv-hergro/' : '/',
   plugins: [react()],
-  preview: {
-    open: '/cv-hergro/'
-  }
+  preview: { open: '/cv-hergro/' },
 }))
