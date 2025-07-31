2ea38ee9b1ff154e6f131e8e7e8cf3d5f1b2850f
AgenorIT <proyectoagenorsoft@gmail.com>
2025-07-31 19:05:58 -0300

    Solucion problema de lectura de archivo css 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  // En DEV base="/", en BUILD base="/cv-hergro/"
   base: command === 'build' ? '/cv-hergro/' : '/',
-  plugins: [react()],
-  // opcional: abre la ruta correcta en preview
-  preview: { open: '/cv-hergro/' }
+  plugins: [react()]
 }))
